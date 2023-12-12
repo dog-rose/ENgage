0064b3234bfb2a0dc262860a3ea037661b2fac64
dog-rose <nessrina.brahim@gmail.com>
2023-12-12 14:13:33 +0100

    seconf commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { SignINComponent } from './signs/sign-in/sign-in.component';
 import { SignUPComponent } from './signs/sign-up/sign-up.component';
 import { AboutUSComponent } from './navbarLinks/about-us/about-us.component';
+import { HomeComponent } from './navbarLinks/home/home.component';
+import { ProductsComponent } from './navbarLinks/products/products.component';
 
 const routes: Routes = [
   {path: 'signIN', component: SignINComponent},
   {path: 'signUP', component: SignUPComponent},
-  {path: 'aboutUS', component: AboutUSComponent}
+  {path: 'aboutUS', component: AboutUSComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'products', component: ProductsComponent}
 ];
 
 @NgModule({
